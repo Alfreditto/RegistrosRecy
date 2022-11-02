cfcf327401dd55270cffe14794b6be252bd05e22
Alfredo Muñoz Lajara <alfredo_lajara@outlook.es>
2022-11-02 16:40:57 +0100

    Arreglado error de diseño
    Al registrarse el campo apellido se veía como una contraseña

--- app/src/main/java/com/example/registrosrecy/MainActivity.kt
+++ app/src/main/java/com/example/registrosrecy/MainActivity.kt
@@ -25,6 +25,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         binding.btnReg.setOnClickListener {
             val intento: Intent = Intent(this, RegistroActivity::class.java)
             resultLauncher.launch(intento)
